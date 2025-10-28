88e8b63a6287a384b48f76b48528246bcb4a8621
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-28 21:27:54 +0300

    Change made to the header

--- moviescope/src/layout/Header.tsx
+++ moviescope/src/layout/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <>
-      <div className="flex justify-between text-white px-20 py-5 text-2xl">
+      <div className="flex justify-between text-white px-20 py-8 text-2xl">
         <p className="text-3xl font-bold text-green-500">🎬MovieScope</p>
         <ul className="flex gap-20 ">
           <li className="hover:bg-gray-700 hover:cursor-pointer py-1 px-3">
