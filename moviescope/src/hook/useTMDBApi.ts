68c28609813845c3777b7c0a694466f7fa5ba6d2
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-28 16:09:38 +0300

    API fetch

---
+++ moviescope/src/hook/useTMDBApi.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+const api_key: string = import.meta.env.VITE_TMDB_API_KEY;
+const api_url: string = import.meta.env.VITE_TMDB_BASE_URL;
+const image_base_url = import.meta.env.IMAGE_BASE_URL;
+const original_image_base_url = import.meta.env.ORIGINAL_IMAGE_BASE_URL;
+
+export async function searchMovies() {
+  try {
+    const res = await axios.get(
+      `${api_url}/trending/movie/week?api_key=${api_key}`
+    );
+    return res.data.result || [];
+  } catch (error) {
+    console.error("Erro occured", error);
+    throw error;
+  }
+}
