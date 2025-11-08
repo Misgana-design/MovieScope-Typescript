ac53c6418635dc57ed1786648ac3634b8c39dfa7
Misgana-design <misganawoldeselassie@gmail.com>
2025-11-08 17:03:01 +0300

    MovieResponse to Movie

--- moviescope/src/hook/useTMDBApi.ts
+++ moviescope/src/hook/useTMDBApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { MovieResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const api_key = import.meta.env.VITE_TMDB_API_KEY;
 const api_url = import.meta.env.VITE_TMDB_BASE_URL;
@@ -7,7 +7,7 @@ export const image_base_url = import.meta.env.IMAGE_BASE_URL;
 export const original_image_base_url = import.meta.env
   .VITE_ORIGINAL_IMAGE_BASE_URL;
 
-export async function searchMovies(): Promise<MovieResponse> {
+export async function searchMovies(): Promise<Movie[]> {
   try {
     const res = await axios.get(
       `${api_url}/trending/movie/week?api_key=${api_key}`
