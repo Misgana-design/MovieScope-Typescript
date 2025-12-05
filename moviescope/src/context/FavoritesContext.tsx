eeabebed5dd07522899e64ebf69621ff77ae4b09
Misgana-design <misganawoldeselassie@gmail.com>
2025-12-05 11:28:34 +0300

    Favorites context after adding type safety

--- moviescope/src/context/FavoritesContext.tsx
+++ moviescope/src/context/FavoritesContext.tsx
@@ -3,25 +3,40 @@ import {
   loadFromLocalStorage,
   saveToLocalStorage,
 } from "../hook/useLocalStorage.ts";
+import type { Movie } from "../types/movie.ts";
+import type { ReactNode } from "react";
 
-function reducer(state, action) {
+type FavoritesAction =
+  | { type: "ADD_FAVORITES"; payload: Movie }
+  | { type: "REMOVE_FAVORITES"; payload: Movie };
+
+function reducer(state: Movie[], action: FavoritesAction): Movie[] {
   switch (action.type) {
     case "ADD_FAVORITES":
       if (state.some((s) => s.id === action.payload.id)) {
         return state;
       }
-      [...state, action.payload];
+      return [...state, action.payload];
     case "REMOVE_FAVORITES":
-      state.filter((s) => s.id !== action.payload.id);
+      return state.filter((s) => s.id !== action.payload.id);
     default:
-      state;
+      return state;
   }
 }
 
-export const FavoritesContext = createContext();
+interface FavoritesValueType {
+  addFavorites: (item: Movie) => void;
+  removeFavorites: (item: Movie) => void;
+  isFavorite: (item: Movie) => boolean;
+  state: Movie[];
+}
 
-export function FavoritesProvider({ children }) {
-  const { state, dispatch } = useReducer(reducer, undefined, () => {
+export const FavoritesContext = createContext<FavoritesValueType | undefined>(
+  undefined
+);
+
+export function FavoritesProvider({ children }: { children: ReactNode }) {
+  const [state, dispatch] = useReducer(reducer, undefined, () => {
     const state = loadFromLocalStorage();
     return state ?? [];
   });
@@ -30,26 +45,36 @@ export function FavoritesProvider({ children }) {
     saveToLocalStorage(state);
   }, [state]);
 
-  function addFavorites(item) {
+  function addFavorites(item: Movie) {
     dispatch({ type: "ADD_FAVORITES", payload: item });
   }
 
-  function removeFavorites(item) {
+  function removeFavorites(item: Movie): void {
     dispatch({ type: "REMOVE_FAVORITES", payload: item });
   }
 
-  function isFavorite(item) {
+  function isFavorite(item: Movie): boolean {
     return state.some((fav) => fav.id === item.id);
   }
 
-  <FavoritesContext.Provider
-    value={(addFavorites, removeFavorites, isFavorite, state)}
-  >
-    {children}
-  </FavoritesContext.Provider>;
+  const values: FavoritesValueType | undefined = {
+    addFavorites,
+    removeFavorites,
+    isFavorite,
+    state,
+  };
+
+  return (
+    <FavoritesContext.Provider value={values}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 }
 
 export function useFavorites() {
   const values = useContext(FavoritesContext);
+  if (!values) {
+    throw new Error("useFavorites must be used within a FavoritesProvider");
+  }
   return values;
 }
