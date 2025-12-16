a8db3907a71c937bbb4532cc3922cda6a11bfb2a
Misgana-design <misganawoldeselassie@gmail.com>
2025-12-16 11:04:17 +0300

    Search bar completed

--- moviescope/src/pages/SearchPage.tsx
+++ moviescope/src/pages/SearchPage.tsx
@@ -1,3 +1,18 @@
+import '../pages/Search.css' 
+
 export function SearchPage() {
-  return <h1>Search</h1>;
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="mt-40">
+          <h1 className="text-white text-6xl search">Search for movies</h1>
+          <input
+            className="mt-3 border-green-500 border-2 rounded-full bg-white px-4 py-1"
+            type="text"
+            placeholder="🔍︎ Search for movies"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
