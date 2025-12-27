89389d1b0ea2ff38dcc53bf2fbd77d4e24268ffd
Misgana-design <misganawoldeselassie@gmail.com>
2025-12-27 15:13:45 +0300

    The search page is not responsive yet

--- moviescope/src/pages/SearchPage.tsx
+++ moviescope/src/pages/SearchPage.tsx
@@ -1,17 +1,72 @@
-import '../pages/Search.css' 
+import { useQuery } from "@tanstack/react-query";
+import { searchMovies } from "../hook/useTMDBApi";
+import { useState } from "react";
+import { original_image_base_url } from "../hook/useTMDBApi";
+import type { Movie } from "../types/movie";
+import "../pages/Search.css";
 
 export function SearchPage() {
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const value = formData.get("search");
+
+    if (typeof value === "string") {
+      setQuery(value.trim());
+    }
+  }
+
+  const { data = [] } = useQuery<Movie[]>({
+    queryKey: ["query", query],
+    queryFn: () => {
+      return searchMovies(query);
+    },
+  });
+
   return (
     <>
       <div className="flex justify-center">
         <div className="mt-40">
           <h1 className="text-white text-6xl search">Search for movies</h1>
-          <input
-            className="mt-3 border-green-500 border-2 rounded-full bg-white px-4 py-1"
-            type="text"
-            placeholder="🔍︎ Search for movies"
-          />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="focus:outline-none mt-3 focus:border-green-500 border-2 rounded-full bg-white px-4 py-1 w-90 h-12"
+              type="text"
+              name="search"
+              placeholder="🔍︎ Search for movies"
+            />
+          </form>
         </div>
+      </div>
+
+      <div>
+        {data.length > 0 && (
+          <>
+            <hr className="border-white border-2 my-8" />
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 px-22 mt-5 ">
+              {data?.length > 0 &&
+                data
+                  ?.filter(
+                    (movie) =>
+                      movie &&
+                      movie.id &&
+                      (movie.poster_path || movie.backdrop_path)
+                  )
+                  .map((movie) => (
+                    <div className="hover:cursor-pointer hover:scale-110 duration-150">
+                      <img
+                        src={original_image_base_url + movie.poster_path}
+                        alt={movie.poster_path}
+                      />
+                    </div>
+                  ))}
+            </div>
+            <hr className="border-white border-2 my-8" />
+          </>
+        )}
       </div>
     </>
   );
